fc5feba458976f8382710e6635862560698ae913
Aryan Bhatia <aryanbhatia14@gmail.com>
2021-02-19 04:24:50 +1100

    Create Room Page

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('rooms', RoomView.as_view())
+    path('rooms', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('join', index),
+    path('create', index)
 ]
